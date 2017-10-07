abbf95b692578532634b941071e80089aaf43d5c
Andre Schmidt <andre.osku.schmidt@gmail.com>
2017-10-07 22:47:16 +0200

    splash test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <qsingleinstance.h>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QApplication a(argc, argv);
     QSingleInstance instance;
+
+    QTimer *timer = new QTimer();
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap("/data/projects/qt/qvolumer/splash.png"));
+    QObject::connect(timer, SIGNAL(timeout()), splash, SLOT(close()));
+    QObject::connect(timer, SIGNAL(timeout()), timer, SLOT(stop()));
 
     //this lambda only gets executed on the first instance
     instance.setStartupFunction([&]() -> int {
@@ -14,8 +22,13 @@ int main(int argc, char *argv[])
     //new instances send their arguments and this signal is received on the main instance
     QObject::connect(&instance, &QSingleInstance::instanceMessage, [&](QStringList args){
         qDebug() << "New Instance:" << args;
-        if(args.size() > 1 && args[1] == "-quit")
+        if(args.size() > 1 && args[1] == "-quit") {
             qApp->quit();
+        }
+        if (!timer->isActive()) {
+            splash->show();
+        }
+        timer->start(1000);
     });
 
     return instance.singleExec();
